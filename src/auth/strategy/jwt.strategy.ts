9c6e8f09e18ce7d9d64f7d03daf6cbe1fd3fe1a5
Abayomi Aishida <yomicorperate@gmail.com>
2023-10-30 23:10:12 +0000

    Authenticating with jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrkey: config.get('JWT_SECRET'),
+    });
+  }
+}
